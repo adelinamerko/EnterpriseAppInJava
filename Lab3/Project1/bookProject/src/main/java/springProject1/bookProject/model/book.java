4a367bf8017429462933afeb432ed1c169201dec
Adela Merko <merkoadela27@gmail.com>
2024-04-09 19:05:33 +0200

    Created the project structure and Book entity class

---
+++ Lab3/Project1/bookProject/src/main/java/springProject1/bookProject/model/book.java
@@ -0,0 +1,60 @@
+package springProject1.bookProject.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Data
+@Entity
+@Table(name="library")
+public class book {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="title" )
+    private String title;
+
+    @Column(name="author")
+    private String author;
+
+    public book() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+}
